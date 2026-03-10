f701aac9b57b1a0b3cb5936433ca8f017f4d1794
Izzatullo-107 <107.izzatullo@example.com>
2026-03-10 19:41:24 +0500

    4.1

---
+++ Projekts/4_1_Exception/4_1_Exception/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _4_1_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
